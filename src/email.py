2315dbaa33098f091fe66381ee2377071a01aba1
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-17 19:49:25 -0500

    Importar paquetes
    Buscar correos

--- src/email.py
+++ src/email.py
@@ -1,4 +1,3 @@
-
 """
 Uso: Email
 Creador: Andrés Hernández Mata
@@ -7,3 +6,17 @@ Python: 3.9.1
 Fecha: 17 Abril 2020
 """
 
+import requests
+import re
+
+url = 'https://www.themoscowtimes.com/page/moscow-times'
+
+response = requests.get(url)
+if response.status_code != 200:
+    exit()
+
+# a set of crawled emails
+regExMail = r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
+new_emails = set(re.findall(regExMail, response.text, re.I))
+for i in new_emails:
+    print(i)
